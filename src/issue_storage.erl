cd3ad48595989e19bbf0c6aaa1fdf4bd9e768842
nmb10 <nmb.ten@gmail.com>
2026-01-12 12:52:43 +0300

    Ignoring timeout error while fetching issues.

--- src/issue_storage.erl
+++ src/issue_storage.erl
@@ -47,7 +47,15 @@ init([]) ->
 
 % Sync methods.
 get_issues() ->
-    gen_server:call(?MODULE, {get_issues}).
+  try gen_server:call(?MODULE, {get_issues})
+  catch exit:{timeout,_} ->
+    % exit:{timeout,_} -> {ok, task_active}
+    % Is running fetch_issues.
+    logger:notice("[issue_storage] timeout error - fetch_issues is running now, returning from cache."),
+    MyIssues = get_from_cache(),
+    FeaturedIssues = config_storage:get(<<"featured_issues">>),
+    {MyIssues, FeaturedIssues}
+  end.
 
 
 % Async method.
@@ -71,6 +79,8 @@ handle_info(fetch_issues, State) ->
     {_, _, CurrentIssue} = State,
 
     logger:notice("[issue_storage] Fetching issues now..."),
+    % For testing long fetches
+    % timer:sleep(8000),
 
     JqlQuery = uri_string:quote(
         "assignee = currentUser() and status in (\"New\", \"To Do\", \"In Progress\")"),
@@ -84,8 +94,6 @@ handle_info(fetch_issues, State) ->
     Headers = [{"Authorization", "Bearer " ++ config_storage:get(<<"jira_bearer">>)}],
     Payload = <<>>,
     Options = [],
-    {ok, StatusCode, _RespHeaders, ClientRef} = hackney:request(
-        Method, URL, Headers, Payload, Options),
 
     % Simplify issue structure
     JiraDomain = config_storage:get(<<"jira_domain">>),
@@ -98,34 +106,45 @@ handle_info(fetch_issues, State) ->
           <<"url">> => <<"https://", JiraDomain/binary, "/browse/", IssueKey/binary>>,
           <<"status">> => maps:get(<<"name">>, maps:get(<<"status">>, Fields))}
     end,
+    % FIXME: Move long fetch to the process to not block the whole issue_storage
+    %  Pid = spawn_link(?MODULE, fetch_issuessome_long_running_task, [State]),
+    HackneyResponse = hackney:request(Method, URL, Headers, Payload, Options),
 
-    MyIssues = case StatusCode of
-        200 ->
-            {ok, ResponseBody} = hackney:body(ClientRef),
-            SearchResponseMap = jiffy:decode(ResponseBody, [return_maps]),
-            IssueList = maps:get(<<"issues">>, SearchResponseMap),
+    MyIssues = case HackneyResponse of
+        {ok, StatusCode, _RespHeaders, ClientRef} ->
+            % Success
+            MyIssues1 = case StatusCode of
+                200 ->
+                    {ok, ResponseBody} = hackney:body(ClientRef),
+                    SearchResponseMap = jiffy:decode(ResponseBody, [return_maps]),
+                    IssueList = maps:get(<<"issues">>, SearchResponseMap),
 
-            MyIssues0 = lists:map(Fun, IssueList),
-            Dump = jiffy:encode(MyIssues0, [pretty]),
-            FileName = '/tmp/jtrack-issues-cache.json',
-            filelib:ensure_dir(FileName),
-            file:write_file(FileName, Dump),
-            MyIssues0;
-        _ ->
-            % FIXME: Notify frontend about the problem with issue upgrade.
-            logger:notice(
-                "[issue_storage] Failed to fetch issues. Returning issues from previous success response (if exists). StatusCode: ~p\n",
-                [StatusCode]),
-
-            FileName = '/tmp/jtrack-issues-cache.json',
-            MyIssues0 = case filelib:is_regular(FileName) of
-                true ->
-                    {ok, BinaryData} = file:read_file(FileName),
-                    jiffy:decode(BinaryData, [return_maps]);
-                false ->
-                    []
+                    MyIssues0 = lists:map(Fun, IssueList),
+                    Dump = jiffy:encode(MyIssues0, [pretty]),
+                    FileName = '/tmp/jtrack-issues-cache.json',
+                    filelib:ensure_dir(FileName),
+                    file:write_file(FileName, Dump),
+                    MyIssues0;
+                _ ->
+                    % FIXME: Notify frontend about the problem with issue upgrade.
+                    logger:notice(
+                        "[issue_storage] Failed to fetch issues. Returning issues from previous success response (if exists). StatusCode: ~p\n",
+                        [StatusCode]),
+                    get_from_cache()
             end,
-            MyIssues0
+            MyIssues1;
+        {error, connect_timeout} ->
+            %% Handle connection timeout specifically
+            logger:error("Connection timed out.~n"),
+            get_from_cache();
+        {error, timeout} ->
+            %% Handle receive (response body) timeout specifically
+            logger:error("Receive timed out.~n"),
+            get_from_cache();
+        {error, Reason} ->
+            %% Handle other general errors
+            logger:error("Hackney request failed: ~p~n", [Reason]),
+            get_from_cache()
     end,
     FeaturedIssues = config_storage:get(<<"featured_issues">>),
     logger:notice("[issue_storage] Issues fetching finished."),
@@ -133,3 +152,15 @@ handle_info(fetch_issues, State) ->
     erlang:send_after(1 * 60 * 1000, self(), fetch_issues),
     NewState = {FeaturedIssues, MyIssues, CurrentIssue},
     {noreply, NewState}.
+
+
+get_from_cache() ->
+    FileName = '/tmp/jtrack-issues-cache.json',
+    MyIssues0 = case filelib:is_regular(FileName) of
+        true ->
+            {ok, BinaryData} = file:read_file(FileName),
+            jiffy:decode(BinaryData, [return_maps]);
+        false ->
+            []
+    end,
+    MyIssues0.
